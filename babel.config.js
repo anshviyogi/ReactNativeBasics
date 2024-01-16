4555dbff85c7fffe5b026609795a28269dd447e9
Ansh Viyogi <auni.viyogi@gmail.com>
2024-01-16 11:37:06 +0530

    Native App

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 };
+// just like we use Webpack in React JS project to combine multiple file to one
+// Here, in React Native we use Metro bundler
